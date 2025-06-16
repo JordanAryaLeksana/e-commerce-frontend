b3de0fb30c2400b6516d649546ac4be1720affa2
JordanAryaLeksana <jordanaryaleksana41@gmail.com>
2025-06-17 02:45:32 +0700

    prodcution

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import { store } from "@/store/store";
 import axios from "axios";
 import Cookies from "js-cookie";
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: process.env.NEXT_API_PUBLIC_URL_PRODUCTION,
   headers: {
     "Content-Type": "application/json",
   },
